a6a4a149d1b31a916403bc10b258068b51acdf2f
ongtrumduy <phamhuyduy99@gmail.com>
2021-05-16 10:26:29 +0700

    làm phần hiển thị bộ đề bài tập

--- FRONTEND/src/Components/Content/UserPage/Excercises/ExcerciseAllContent/ExcercisesPublicList.js
+++ FRONTEND/src/Components/Content/UserPage/Excercises/ExcerciseAllContent/ExcercisesPublicList.js
@@ -82,65 +82,285 @@ export default class ExcercisesPublicList extends React.Component {
             { MemberID: "569b6b18-e403-4f34-990a-ebc6f90f2171" },
             { MemberID: "f41fdbc0-0ca7-4777-8d05-9c0fee8900c3" }
           ]
+        },
+        {
+          ExcerciseID: "84c6b250-9980-11eb-9f65-9b578b443991",
+          ExcerciseMemberAdminID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6",
+          ExcerciseInfor: [
+            {
+              ExcerciseName:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseDescription:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseLogo: "/static/media/ggg.8304c57a.PNG"
+            }
+          ],
+          ExcerciseMember: [
+            { MemberID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6" },
+            { MemberID: "569b6b18-e403-4f34-990a-ebc6f90f2171" },
+            { MemberID: "f41fdbc0-0ca7-4777-8d05-9c0fee8900c3" }
+          ]
+        },
+        {
+          ExcerciseID: "84c6b250-9980-11eb-9f65-9b578b443991",
+          ExcerciseMemberAdminID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6",
+          ExcerciseInfor: [
+            {
+              ExcerciseName:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseDescription:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseLogo: "/static/media/ggg.8304c57a.PNG"
+            }
+          ],
+          ExcerciseMember: [
+            { MemberID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6" },
+            { MemberID: "569b6b18-e403-4f34-990a-ebc6f90f2171" },
+            { MemberID: "f41fdbc0-0ca7-4777-8d05-9c0fee8900c3" }
+          ]
+        },
+        {
+          ExcerciseID: "84c6b250-9980-11eb-9f65-9b578b443991",
+          ExcerciseMemberAdminID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6",
+          ExcerciseInfor: [
+            {
+              ExcerciseName:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseDescription:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseLogo: "/static/media/ggg.8304c57a.PNG"
+            }
+          ],
+          ExcerciseMember: [
+            { MemberID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6" },
+            { MemberID: "569b6b18-e403-4f34-990a-ebc6f90f2171" },
+            { MemberID: "f41fdbc0-0ca7-4777-8d05-9c0fee8900c3" }
+          ]
+        },
+        {
+          ExcerciseID: "84c6b250-9980-11eb-9f65-9b578b443991",
+          ExcerciseMemberAdminID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6",
+          ExcerciseInfor: [
+            {
+              ExcerciseName:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseDescription:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseLogo: "/static/media/ggg.8304c57a.PNG"
+            }
+          ],
+          ExcerciseMember: [
+            { MemberID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6" },
+            { MemberID: "569b6b18-e403-4f34-990a-ebc6f90f2171" },
+            { MemberID: "f41fdbc0-0ca7-4777-8d05-9c0fee8900c3" }
+          ]
+        },
+        {
+          ExcerciseID: "84c6b250-9980-11eb-9f65-9b578b443991",
+          ExcerciseMemberAdminID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6",
+          ExcerciseInfor: [
+            {
+              ExcerciseName:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseDescription:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseLogo: "/static/media/ggg.8304c57a.PNG"
+            }
+          ],
+          ExcerciseMember: [
+            { MemberID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6" },
+            { MemberID: "569b6b18-e403-4f34-990a-ebc6f90f2171" },
+            { MemberID: "f41fdbc0-0ca7-4777-8d05-9c0fee8900c3" }
+          ]
+        },
+        {
+          ExcerciseID: "84c6b250-9980-11eb-9f65-9b578b443991",
+          ExcerciseMemberAdminID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6",
+          ExcerciseInfor: [
+            {
+              ExcerciseName:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseDescription:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseLogo: "/static/media/ggg.8304c57a.PNG"
+            }
+          ],
+          ExcerciseMember: [
+            { MemberID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6" },
+            { MemberID: "569b6b18-e403-4f34-990a-ebc6f90f2171" },
+            { MemberID: "f41fdbc0-0ca7-4777-8d05-9c0fee8900c3" }
+          ]
+        },
+        {
+          ExcerciseID: "4728a270-9979-11eb-9f65-9b578b443991",
+          ExcerciseMemberAdminID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6",
+          ExcerciseInfor: [
+            {
+              ExcerciseName: "kiến tạo quốc viện",
+              ExcerciseDescription: "kiến tạo lục viện",
+              ExcerciseLogo: "/static/media/kkk.16477550.PNG"
+            }
+          ],
+          ExcerciseMember: [
+            { MemberID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6" }
+          ]
+        },
+        {
+          ExcerciseID: "1c130670-997e-11eb-9f65-9b578b443991",
+          ExcerciseMemberAdminID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6",
+          ExcerciseInfor: [
+            {
+              ExcerciseName: "Kiến tạo chánh phủ",
+              ExcerciseDescription: "Kiến tạo chánh phủ trung ương",
+              ExcerciseLogo: "/static/media/lll.cba69ff3.PNG"
+            }
+          ],
+          ExcerciseMember: [
+            { MemberID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6" }
+          ]
+        },
+        {
+          ExcerciseID: "37173950-997e-11eb-9f65-9b578b443991",
+          ExcerciseMemberAdminID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6",
+          ExcerciseInfor: [
+            {
+              ExcerciseName: "Suy tôn đại hòang đế đại đế quốc",
+              ExcerciseDescription: "suy tôn đại đế",
+              ExcerciseLogo: "/static/media/aaa.a1db6d38.PNG"
+            }
+          ],
+          ExcerciseMember: [
+            { MemberID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6" },
+            { MemberID: "569b6b18-e403-4f34-990a-ebc6f90f2171" }
+          ]
+        },
+        {
+          ExcerciseID: "84c6b250-9980-11eb-9f65-9b578b443991",
+          ExcerciseMemberAdminID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6",
+          ExcerciseInfor: [
+            {
+              ExcerciseName:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseDescription:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseLogo: "/static/media/ggg.8304c57a.PNG"
+            }
+          ],
+          ExcerciseMember: [
+            { MemberID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6" },
+            { MemberID: "569b6b18-e403-4f34-990a-ebc6f90f2171" },
+            { MemberID: "f41fdbc0-0ca7-4777-8d05-9c0fee8900c3" }
+          ]
+        },
+        {
+          ExcerciseID: "84c6b250-9980-11eb-9f65-9b578b443991",
+          ExcerciseMemberAdminID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6",
+          ExcerciseInfor: [
+            {
+              ExcerciseName:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseDescription:
+                "Xây dựng thái bình thịnh trị ấm no cho muôn dân đế quốc",
+              ExcerciseLogo: "/static/media/ggg.8304c57a.PNG"
+            }
+          ],
+          ExcerciseMember: [
+            { MemberID: "6a6afc28-167d-4d7d-bb4d-6285b195aec6" },
+            { MemberID: "569b6b18-e403-4f34-990a-ebc6f90f2171" },
+            { MemberID: "f41fdbc0-0ca7-4777-8d05-9c0fee8900c3" }
+          ]
         }
       ],
-      NumberExcerciseOnPage: "4",
+      NumberExcerciseOnPage: "3",
+      NumberIndexExcerciseOnPage: "5",
+      CurrentIndexExcercisePage: "1",
+      CurrentIndexOfIndexExcercisePage: "1",
       AllNumberOfExcerciseOnPageList: []
     };
   }
 
-  // componentDidMount = () => {
-  //   axios
-  //     .post("/getexcercisepublixclist", {
-  //       MemberID: this.props.MemberID
-  //     })
-  //     .then(res => {
-  //       // console.log(res.data);
-  //       this.setState({
-  //         AllExcercisePublicList: res.data
-  //       });
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // };
-
-  chooseOneJoinedExcercise = ExcerciseID => {
-    this.props.getExcerciseIDMemberChoice(ExcerciseID);
-    this.props.updateRenderExcerciseControl("excercisecontent");
-  };
-
   componentDidMount = () => {
     const allNumberOfExcerciseOnPageList = [];
     const ExcerciseListLength = this.state.AllExcercisePublicList.length;
-    // console.log("ra số phần tử ", ExcerciseListLength);
-    // console.log(
-    //   "Ra thử xem nó là gì ",
-    let allNumberOfExcercise = Math.ceil(
+
+    const allNumberOfExcercise = Math.ceil(
       ExcerciseListLength / Number(this.state.NumberExcerciseOnPage)
     );
-    // );
+
     for (let i = 1; i <= allNumberOfExcercise; i++) {
       allNumberOfExcerciseOnPageList.push(i);
-      console.log("Ra nhá");
     }
-    console.log("RA allNumber", allNumberOfExcerciseOnPageList);
     this.setState({
       AllNumberOfExcerciseOnPageList: allNumberOfExcerciseOnPageList
     });
   };
 
+  chooseIndexExcercisePage = event => {
+    this.setState({
+      CurrentIndexExcercisePage: Number(event.target.id)
+    });
+  };
+
   renderIndexOfExcerciseItemList = () => {
-    this.state.AllNumberOfExcerciseOnPageList.map(numberindexitem => (
-      <li key={numberindexitem} id={numberindexitem} className="active">
-        {numberindexitem} akjksjgkakgjakjgksajD
-      </li>
-    ));
+    const currentIndexOfIndexExcercisePage = Number(
+      this.state.CurrentIndexOfIndexExcercisePage
+    );
+    const numberIndexExcerciseOnPage = Number(
+      this.state.NumberIndexExcerciseOnPage
+    );
+
+    const indexOfLastIndexExcerciseList =
+      currentIndexOfIndexExcercisePage * numberIndexExcerciseOnPage;
+
+    const indexOfFirstIndexExcerciseList =
+      indexOfLastIndexExcerciseList - numberIndexExcerciseOnPage;
+
+    const currentIndexOfChoiceIndexExcerciseList = this.state.AllNumberOfExcerciseOnPageList.slice(
+      indexOfFirstIndexExcerciseList,
+      indexOfLastIndexExcerciseList
+    );
+
+    const allNumberOfIndexOfExcercise = Math.ceil(
+      this.state.AllNumberOfExcerciseOnPageList.length /
+        numberIndexExcerciseOnPage
+    );
+
+    console.log("RA thằng cu này xem sao ", allNumberOfIndexOfExcercise);
+    if (allNumberOfIndexOfExcercise === 1) {
+      return (
+        <div className="user-excercises_all__public-list___control____index-item">
+          {currentIndexOfChoiceIndexExcerciseList.map(numberindexitem => (
+            <div
+              key={numberindexitem}
+              id={numberindexitem}
+              onClick={event => this.chooseIndexExcercisePage(event)}
+            >
+              {numberindexitem}
+            </div>
+          ))}
+        </div>
+      );
+    } else {
+      return (
+        <div className="user-excercises_all__public-list___control____index-item">
+          {currentIndexOfChoiceIndexExcerciseList.map(numberindexitem => (
+            <div
+              key={numberindexitem}
+              id={numberindexitem}
+              onClick={event => this.chooseIndexExcercisePage(event)}
+            >
+              {numberindexitem}
+            </div>
+          ))}
+          <span>...</span>
+        </div>
+      );
+    }
   };
 
   selectIndexForRenderExcerciseItem = () => {
     return (
-      <div className="user-excercises_create-new__QandA___control">
+      <div className="user-excercises_all__public-list___control">
         <div>
           <i
             style={
@@ -154,7 +374,7 @@ export default class ExcercisesPublicList extends React.Component {
             &#xe5c4;
           </i>
         </div>
-        <div>{this.renderIndexOfExcerciseItemList()}</div>
+        {this.renderIndexOfExcerciseItemList()}
         <div>
           <i
             style={
@@ -168,13 +388,49 @@ export default class ExcercisesPublicList extends React.Component {
             &#xe5c8;
           </i>
         </div>
-        <div>
-          <input
-            type="button"
-            value="Hoàn tất"
-            onClick={() => this.sendToCompleteExcercises()}
-          />
-        </div>
+      </div>
+    );
+  };
+
+  renderChooseIndexExcercisePublicList = () => {
+    const currentIndexExcercisePage = Number(
+      this.state.CurrentIndexExcercisePage
+    );
+    const numberExcerciseOnPage = Number(this.state.NumberExcerciseOnPage);
+
+    const indexOfLastExcerciseList =
+      currentIndexExcercisePage * numberExcerciseOnPage;
+
+    const indexOfFirstExcerciseList =
+      indexOfLastExcerciseList - numberExcerciseOnPage;
+
+    const currentChoiceIndexExcerciseList = this.state.AllExcercisePublicList.slice(
+      indexOfFirstExcerciseList,
+      indexOfLastExcerciseList
+    );
+
+    return (
+      <div className="user-excercises_all__public-list___choice-index-content">
+        {currentChoiceIndexExcerciseList.map(excerciseitem =>
+          excerciseitem.ExcerciseInfor.map(
+            (excercisenameitem, excerciseindex) => (
+              <div key={excerciseindex}>
+                <img
+                  style={{
+                    height: "120px",
+                    width: "120px",
+                    margin: "32px 0 0 0"
+                  }}
+                  alt="team-logo"
+                  src={de111}
+                />
+                <p style={{ fontWeight: "bold" }}>
+                  {excercisenameitem.ExcerciseName}
+                </p>
+              </div>
+            )
+          )
+        )}
       </div>
     );
   };
@@ -185,39 +441,7 @@ export default class ExcercisesPublicList extends React.Component {
         <div className="user-excercises_all__public-list___title">
           <p>Bộ đề - Bài tập Công khai</p>
         </div>
-        <div className="user-excercises_all__public-list___content">
-          {/* {this.state.AllExcercisePublicList.map((excerciseitem, Excerciseindex) =>
-          Excerciseitem.ExcerciseInfor.map(Excercisenameitem => (
-            <ExcercisesItem
-              key={Excerciseindex}
-              ExcerciseID={excerciseitem.ExcerciseID}
-              ExcerciseLogo={excerciseitem.ExcerciseLogo}
-              ExcerciseName={excerciseitem.ExcerciseName}
-              chooseOneJoinedExcercise={this.chooseOneJoinedExcercise}
-            />
-          ))
-        )} */}
-          {this.state.AllExcercisePublicList.map(excerciseitem =>
-            excerciseitem.ExcerciseInfor.map(
-              (excercisenameitem, excerciseindex) => (
-                <div key={excerciseindex}>
-                  <img
-                    style={{
-                      height: "120px",
-                      width: "120px",
-                      margin: "32px 0 0 0"
-                    }}
-                    alt="team-logo"
-                    src={de111}
-                  />
-                  <p style={{ fontWeight: "bold" }}>
-                    {excercisenameitem.ExcerciseName}
-                  </p>
-                </div>
-              )
-            )
-          )}
-        </div>
+        {this.renderChooseIndexExcercisePublicList()}
         {this.selectIndexForRenderExcerciseItem()}
       </div>
     );
