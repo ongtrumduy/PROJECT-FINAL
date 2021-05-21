3b60e97cff2884766fb755c8eced01076948faf7
ongtrumduy <phamhuyduy99@gmail.com>
2021-05-22 00:28:15 +0700

    làm phần note và assignment

--- FRONTEND/src/Components/Content/UserPage/Excercises/ExcercisesCreateNew/ExcercisesCreateNewMain.js
+++ FRONTEND/src/Components/Content/UserPage/Excercises/ExcercisesCreateNew/ExcercisesCreateNewMain.js
@@ -147,7 +147,7 @@ export default class RemindersCreateNew extends React.Component {
         <div
           className="user-excercises_create-new__main___backtoexcerciseall"
           onClick={() =>
-            this.props.updateRenderExcerciseControl("excerciseall")
+            this.props.updateRenderExcerciseControl("exc7erciseall")
           }
         >
           <div>
